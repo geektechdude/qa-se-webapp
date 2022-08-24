f6bc3f41aaefe65447d22b87c1556ce192484ca1
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2022-08-24 16:59:19 +0100

    Updated schema.sql
    Updated schema.sql to create asset table

--- flaskwebapp/schema.sql
+++ flaskwebapp/schema.sql
@@ -1,7 +1,19 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS asset;
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
+);
+
+CREATE TABLE asset (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    serial_number TEXT UNIQUE NOT NULL,
+    device_model TEXT NOT NULL,
+    purchase_date DATE NOT NULL,
+    warranty_exp_date DATE NOT NULL,
+    assigned_to TEXT NOT NULL,
+    retired BOOLEAN,
+    retirment_date DATE
 );
